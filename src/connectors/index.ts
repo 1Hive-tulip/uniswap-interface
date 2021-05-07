58d94d5f131dcc5459e7542c5e48bd7835690e34
Daniel Vega <dvega@tangocode.com>
2021-05-07 15:17:21 -0300

    add swapr code

---
+++ src/connectors/index.ts
@@ -0,0 +1,33 @@
+import { InjectedConnector } from '@web3-react/injected-connector'
+import { AuthereumConnector } from '@web3-react/authereum-connector'
+import { WalletConnectConnector } from '@web3-react/walletconnect-connector'
+import { CustomNetworkConnector } from './CustomNetworkConnector'
+import { ChainId } from 'dxswap-sdk'
+
+export const INFURA_PROJECT_ID = '0ebf4dd05d6740f482938b8a80860d13'
+
+export const network = new CustomNetworkConnector({
+  urls: {
+    [ChainId.MAINNET]: `https://mainnet.infura.io/v3/${INFURA_PROJECT_ID}`,
+    [ChainId.XDAI]: 'https://rpc.xdaichain.com/',
+    [ChainId.MATIC]: 'https://rpc-mainnet.maticvigil.com/'
+  },
+  defaultChainId: ChainId.MAINNET
+})
+
+export const injected = new InjectedConnector({
+  supportedChainIds: [ChainId.MAINNET, ChainId.RINKEBY, ChainId.ARBITRUM_TESTNET_V3, ChainId.SOKOL, ChainId.XDAI, ChainId.MATIC]
+})
+
+// mainnet only
+export const walletConnect = new WalletConnectConnector({
+  rpc: {
+    1: `https://mainnet.infura.io/v3/${INFURA_PROJECT_ID}`
+  },
+  bridge: 'https://bridge.walletconnect.org',
+  qrcode: true,
+  pollingInterval: 15000
+})
+
+// mainnet only
+export const authereum = new AuthereumConnector({ chainId: 1 })
